912bbd0aa33b21ae048b7d3c133243df6b8a4e74
ohdong9795 <ohdong9795@naver.com>
2021-06-02 00:34:09 +0900

    slick 캐러셀 테스트

--- src/component/Testgrid.js
+++ src/component/Testgrid.js
@@ -1,38 +1,48 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import '../../node_modules/slick-carousel/slick/slick.css'
+import '../../node_modules/slick-carousel/slick/slick-theme.css'
 
 export default class Testgrid extends Component {
   render() {
+    const styles = {
+      text: {
+        textAlign:"center"
+      }
+    }
     const settings = {
       dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
     };
     return (
       <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
+        <center>
+          <Card style={{width:"500px", background: "#419be0"}} variant="outlined">
+            <CardContent>
+              <Slider style={{width: "400px"}} {...settings}>
+                <div>
+                  <h3 style={styles.text}>1</h3>
+                </div>
+                <div>
+                  <h3 style={styles.text}>2</h3>
+                </div>
+                <div>
+                  <h3 style={styles.text}>3</h3>
+                </div>
+                <div>
+                  <h3 style={styles.text}>4</h3>
+                </div>
+                <div>
+                  <h3 style={styles.text}>5</h3>
+                </div>
+                <div>
+                  <h3 style={styles.text}>6</h3>
+                </div>
+              </Slider>
+            </CardContent>
+          </Card>
+        </center>
       </div>
     );
   }
